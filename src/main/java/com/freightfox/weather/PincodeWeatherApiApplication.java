7efab8a4066c936401ab5190b0d75b11a8a07c75
KishanHegade <kishanhegade@gmail.com>
2024-10-24 20:52:50 +0530

    Initial commit

---
+++ src/main/java/com/freightfox/weather/PincodeWeatherApiApplication.java
@@ -0,0 +1,13 @@
+package com.freightfox.weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PincodeWeatherApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PincodeWeatherApiApplication.class, args);
+	}
+
+}
