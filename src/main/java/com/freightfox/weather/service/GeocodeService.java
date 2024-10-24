ed0394c4a0e943dcefafb4b3b18ad2e0ff0e3888
KishanHegade <kishanhegade@gmail.com>
2024-10-24 23:25:39 +0530

    Modify GeocodeService

--- src/main/java/com/freightfox/weather/service/GeocodeService.java
+++ src/main/java/com/freightfox/weather/service/GeocodeService.java
@@ -1,7 +1,6 @@
 package com.freightfox.weather.service;
 
 import com.freightfox.weather.dto.GeocodeResponse;
-import com.freightfox.weather.dto.LatLng;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -16,15 +15,13 @@ public class GeocodeService {
     private String apiKey;
     private final RestTemplate restTemplate;
 
-    public LatLng getLatLngFromPincode(String pincode) {
+    public GeocodeResponse getLatLngFromPincode(String pincode) {
         String url = UriComponentsBuilder.fromHttpUrl("https://api.openweathermap.org/geo/1.0/zip")
                 .queryParam("zip", pincode+ "," + "IN")
                 .queryParam("appid", apiKey)
                 .toUriString();
 
-        GeocodeResponse response = restTemplate.getForObject(url, GeocodeResponse.class);
-
-        return new LatLng(response.getLat(), response.getLon());
+        return restTemplate.getForObject(url, GeocodeResponse.class);
     }
 
 }
