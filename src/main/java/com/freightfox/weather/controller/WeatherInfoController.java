d5729438086cde439a09a128797a3e3c07c7b6a2
KishanHegade <kishanhegade@gmail.com>
2024-10-25 00:19:22 +0530

    Modify WeatherInfoController class

--- src/main/java/com/freightfox/weather/controller/WeatherInfoController.java
+++ src/main/java/com/freightfox/weather/controller/WeatherInfoController.java
@@ -1,7 +1,12 @@
 package com.freightfox.weather.controller;
 
+import com.freightfox.weather.dto.WeatherInfoResponse;
 import com.freightfox.weather.service.WeatherInfoService;
+import com.freightfox.weather.utility.AppResponseBuilder;
+import com.freightfox.weather.utility.ResponseStructure;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,9 +19,11 @@ import java.time.LocalDate;
 public class WeatherInfoController {
 
     private WeatherInfoService weatherInfoService;
+    private AppResponseBuilder builder;
     @GetMapping("/api/weather/{pincode}")
-    public String fetchWeatherByPincode(@PathVariable String pincode,@RequestParam LocalDate date)
+    public ResponseEntity<ResponseStructure<WeatherInfoResponse>> fetchWeatherByPincode(@PathVariable String pincode, @RequestParam LocalDate date)
     {
-        return null;
+        WeatherInfoResponse weatherInfoResponse = weatherInfoService.fetchWeatherByPincode(pincode,date);
+        return builder.success(HttpStatus.FOUND, "Weather Info found", weatherInfoResponse);
     }
 }
